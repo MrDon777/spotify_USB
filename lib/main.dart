6c27141c4cb12b0e1574455bc407abc7b2b32253
javiersmash <javiersmash1@gmail.com>
2025-09-03 14:58:32 -0500

    sd
    sdsd

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Spotify-like UI Demo',
+      title: 'Spotify-like UI Demooo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: spotifyBlack,
